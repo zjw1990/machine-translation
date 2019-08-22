b865e1b5fef33dff2ae397688b86b8a49d37e09d
zjw1990 <zhaojwvsmike@outlook.com>
2019-08-22 14:02:06 +1200

    padding

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,10 @@
 import re
 import unicodedata
 import torch
+import numpy as np
 SOS_token = 0
 EOS_token = 1
+MAX_LENGTH = 30
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 def normalizeString(s):
@@ -19,18 +21,24 @@ def unicodeToAscii(s):
     )
 
 
-
+# transfer words in languages into indexes
 def indexesFromSentence(lang, sentence):
-    return [lang.word2idx[word] for word in sentence.split(' ')]
+    senence_indexes = [lang.word2idx[word] for word in sentence.split(' ')] # a list with length of input
+    return senence_indexes
 
-
+# transfer the index into a tensor, ,padding, add a eos token
 def tensorFromSentence(lang, sentence):
     indexes = indexesFromSentence(lang, sentence)
     indexes.append(EOS_token)
-    return torch.tensor(indexes, dtype=torch.long, device = device).view(-1, 1)
+    padded_indexes = np.zeros(MAX_LENGTH)
+    for i in range(len(indexes)):
+        padded_indexes[i] = indexes[i]
+    sentence_tensor = torch.tensor(padded_indexes, dtype=torch.long, device = device).view(-1, 1)
+    return sentence_tensor
 
 
 def tensorsFromPair(pair, input_lang, output_lang):
+    
     input_tensor = tensorFromSentence(input_lang, pair[0])
     target_tensor = tensorFromSentence(output_lang, pair[1])
     return (input_tensor, target_tensor)
